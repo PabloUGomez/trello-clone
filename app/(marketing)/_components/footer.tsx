313a3746e6b41022e308b868d4c9956832e3ae8e
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-14 06:10:34 -0300

    add middleware with clerk

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
       <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
         <TrelloIcon />
         <div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
-          <Button size='sm' variant='ghost'>@Privacy Policy</Button>
+          <Button size='sm' variant='ghost'>Privacy Policy</Button>
           <Button size='sm' variant='ghost'>Terms of Service</Button>
         </div>
       </div>

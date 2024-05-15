48555e9b83b141b9ef96b3d5a9a95fdb5acc4d4e
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-15 09:57:19 -0300

    config middleware to redirect and protect routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,29 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/signin',
+  '/signup',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (auth().userId && isPublicRoute(req)) {
+    let path = '/select-org';
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+    const orgSelection = new URL(path, req.url).toString();
+    return NextResponse.redirect(orgSelection);
+  }
+  if (!auth().userId && !isPublicRoute(req)) {
+    return auth().redirectToSignIn();
+  }
+  if (auth().userId && !auth().orgId && req.nextUrl.pathname !== '/select-org') {
+    const orgSelection = new URL('/select-org', req.url).toString();
+    return NextResponse.redirect(orgSelection);
+  }
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],

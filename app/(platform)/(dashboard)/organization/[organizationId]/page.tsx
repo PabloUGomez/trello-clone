1b8e5ad15d5daacd3cd85bacd3e267e1859fcc6d
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-20 14:37:09 -0300

    add prisma and turso

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,38 @@
+import { prisma } from '@/lib/db'
 
 const OrganizationPage = () => {
+  async function createBoard(formData: FormData) {
+    'use server'
+
+    const title = formData.get('title') as string
+    if (!title) {
+      throw new Error('Title is required')
+    }
+
+    try {
+      const board = await prisma.board.create({
+        data: {
+          title: title,
+        },
+      })
+      console.log('Board created:', board)
+    } catch (error) {
+      console.error('Error creating board:', error)
+    }
+  }
+
   return (
     <div>
-      <h1>Organization Page</h1>
+      <form action={createBoard}>
+        <input
+          type='text'
+          id='title'
+          name='title'
+          required
+          placeholder='Enter Board title'
+          className='border-black'
+        />
+      </form>
     </div>
   )
 }

97cef0878575eff0f65488ee541399541f012da9
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-21 14:28:51 -0300

    fix useFormState error

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,32 +1,35 @@
 'use client'
 
-import { createBoard } from '@/actions/createBoard'
+import { createBoard, State } from '@/actions/createBoard'
 import { Button } from '@/components/ui/button'
 import { useFormState } from 'react-dom'
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} }
-  const [state, dispatch] = useFormState(createBoard, initialState)
+  const initialState = { message: '', errors: { title: [] } }
+  const [state, dispatch] = useFormState(
+    (prevState: State, formData: FormData) => createBoard(prevState, formData),
+    initialState
+  )
 
   return (
     <div className='flex flex-col space-y-4'>
       <form action={dispatch}>
         <div className='flex flex-col space-y-2'>
-        <input
-          type='text'
-          id='title'
-          name='title'
-          required
-          placeholder='Enter Board title'
-          className='border-black'
-        />
-        {state?.errors?.title && (
-          <div className='text-rose-500'>
-            {state.errors.title.map((error : string) => (
-              <div key={error}>{error}</div>
-            ))}
-          </div>
-        )}
+          <input
+            type='text'
+            id='title'
+            name='title'
+            required
+            placeholder='Enter Board title'
+            className='border-black'
+          />
+          {state?.errors?.title && (
+            <div className='text-rose-500'>
+              {state.errors.title.map((error: string) => (
+                <div key={error}>{error}</div>
+              ))}
+            </div>
+          )}
         </div>
         <Button type='submit'>Submit</Button>
       </form>

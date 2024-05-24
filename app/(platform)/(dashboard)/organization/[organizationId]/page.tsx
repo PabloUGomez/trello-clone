ba7ebf90eba981a447449f36da755ff59e76b7d6
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-24 17:37:05 -0300

    add boardlist

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,14 @@
-import { createBoard } from '@/actions/create-board'
-import { Button } from '@/components/ui/button'
-import { prisma } from '@/lib/db'
-import { Form } from './form'
+import { Separator } from '@/components/ui/separator'
+import { Info } from './_components/info'
+import { BoardList } from './_components/board-list'
 
 const OrganizationPage = async () => {
-  const boards = await prisma.board.findMany()
-
   return (
-    <div className='flex flex-col space-y-4'>
-      <Form />
-      <div className='space-y-2'>
-        {boards.map((board) => (
-          <div key={board.id} className='flex justify-between'>
-            <div>Board name:{board.title}</div>
-          </div>
-        ))}
+    <div className='w-full mb-20'>
+      <Info />
+      <Separator className='my-4' />
+      <div className='px-2 md:px-4'>
+        <BoardList/>
       </div>
     </div>
   )

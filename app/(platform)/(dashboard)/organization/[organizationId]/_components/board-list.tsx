00da37a4a9291d6878dc9b6d9826a6ccab65bb5c
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-28 18:28:05 -0300

    add boardList Skeleton

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -5,6 +5,7 @@ import { prisma } from '@/lib/db'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export const BoardList = async () => {
   const { orgId } = auth()
@@ -56,6 +57,21 @@ export const BoardList = async () => {
           </div>
         </FormPopover>
       </div>
+    </div>
+  )
+}
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
     </div>
   )
 }

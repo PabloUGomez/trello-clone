aa1b7d6a21404f53379e32a4fce12ed980221338
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-31 23:04:21 -0300

    add list conteiner, wrapper and form in boardPage

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,46 @@
-const BoardIdPage = () => {
+import { auth } from '@clerk/nextjs/server'
+
+import { prisma } from '@/lib/db'
+import { redirect } from 'next/navigation'
+import { ListContainer } from './_components/list-container'
+interface BoardIdPageProps {
+  params: {
+    boardId: string
+  }
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth()
+
+  if (!orgId) {
+    redirect('/select-org')
+  }
+
+  const lists = await prisma.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  })
+
   return (
-    <div>
-      <h1>BoardIdPage</h1>
+    <div className='p-4 h-full overflow-x-auto'>
+      <ListContainer
+        boardId={params.boardId}
+        data={lists}
+      />
     </div>
   )
 }

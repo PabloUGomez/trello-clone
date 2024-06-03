eeac731d34087ed2a30ad3a5a2d1f1a23743e053
Pablo Gomez <pablogomez0430@gmail.com>
2024-06-02 21:46:52 -0300

    add list item and container

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from '@/types'
 import { ListForm } from './list-form'
+import { useEffect, useState } from 'react'
+import { ListItem } from './list-item'
 
 interface ListContainerProps {
   boardId: string
@@ -9,8 +11,17 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+  const [orderData, setOrderData] = useState(data)
+
+  useEffect(() => {
+    setOrderData(data)
+  }, [data])
+
   return (
-    <ol>
+    <ol className='flex gap-x-3 h-full'>
+      {orderData.map((list, index) => (
+        <ListItem key={list.id} data={list} index={index} />
+      ))}
       <ListForm />
       <div className='flex-shrink-0 w-1' />
     </ol>

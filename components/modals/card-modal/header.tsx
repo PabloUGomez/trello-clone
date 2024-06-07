2ccf0f044116ded25a887c7c68f5edcb5df3b67e
Pablo Gomez <pablogomez0430@gmail.com>
2024-06-07 07:19:27 -0300

    add update card action

---
+++ components/modals/card-modal/header.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { FormInput } from '@/components/form/form-input'
+import { Skeleton } from '@/components/ui/skeleton'
+import { CardWithList } from '@/types'
+import { Layout } from 'lucide-react'
+import { ElementRef, useRef, useState } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import { useParams } from 'next/navigation'
+
+interface HeaderProps {
+  data: CardWithList
+}
+
+export const Header = ({ data }: HeaderProps) => {
+  const [title, setTitle] = useState(data.title)
+  const queryClient = useQueryClient()
+  const params = useParams()
+  const inputRef = useRef<ElementRef<'input'>>(null)
+  const onBlur = () => {
+    inputRef.current?.form?.submit()
+  }
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+  }
+
+  return (
+    <div className='flex items-start gap-x-3 mb-6 w-full'>
+      <Layout className='h-5 w-5 mt-1 text-neutral-700' />
+      <div className='w-full'>
+        <form action={onSubmit}>
+          <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
+            id='title'
+            defaultValue={data.title}
+            className='font-semibold px-1 text-xl bg-transparent border-transparent text-neutral-700 relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate'
+          />
+        </form>
+        <p className='text-sm text-muted-foreground'>
+          in list <span className='underline'>{data.list.title}</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+Header.Skeleton = function HeaderSkeleton() {
+  return (
+    <div className='flex items-start gap-x-3 mb-6'>
+      <Skeleton className='h-6 w-6 mt-1 bg-neutral-200' />
+      <div>
+        <Skeleton className='h-6 w-24 mb-1 bg-neutral-200' />
+        <Skeleton className='h-4 w-12 bg-neutral-200' />
+      </div>
+    </div>
+  )
+}

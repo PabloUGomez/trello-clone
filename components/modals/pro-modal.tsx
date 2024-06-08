ccb0dd1b9a19a4c2faec242c26bfbb06d3ce8197
Pablo Gomez <pablogomez0430@gmail.com>
2024-06-08 11:38:06 -0300

    add the redirect action to proModal

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -4,9 +4,25 @@ import { useProModal } from '@/hooks/use-pro-modal'
 import { Dialog, DialogContent } from '../ui/dialog'
 import { Button } from '../ui/button'
 import Image from 'next/image'
+import { useAction } from '@/hooks/use-action'
+import { stripeRedirect } from '@/actions/stripe-redirect'
+import { toast } from 'sonner'
 
 export const ProModal = () => {
   const { isOpen, onOpen, onClose } = useProModal()
+  const { execute, isLoading } = useAction(stripeRedirect, {
+    onSuccess: (data) => {
+      window.location.href = data
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
+  const onClick = () => {
+    execute({})
+  }
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className='max-w-md p-0 overflow-hidden'>
@@ -33,7 +49,12 @@ export const ProModal = () => {
               <li>Add more!</li>
             </ul>
           </div>
-          <Button className='w-full' variant='primary'>
+          <Button
+            className='w-full'
+            variant='primary'
+            disabled={isLoading}
+            onClick={onClick}
+          >
             Upgrade to Pro
           </Button>
         </div>
